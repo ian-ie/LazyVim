3869150461cb8dd15c5cf64c2527de49d3e0f842
ianie <2817181586@qq.com>
2023-11-14 17:39:53 +0800

    leader w

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buff
 vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>bs", "<cmd>BufferLineSortByDirectory<cr>", { desc = "Sort by directory" })
 vim.keymap.set("n", "<leader>bS", "<cmd>BufferLineSortByExtension<cr>", { desc = "Sort by extension" })
+vim.keymap.set("n", "<leader>W", "<cmd>w<cr><esc>", { desc = "Save", remap = true })
 
 vim.keymap.set({ "n", "x" }, "H", "^")
 vim.keymap.set({ "n", "x" }, "L", "$")
