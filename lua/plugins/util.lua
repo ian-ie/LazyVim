d08d259c72c61165829fa27966005fbf02f03102
ianie <2817181586@qq.com>
2024-03-29 16:45:03 +0800

    chage bookmark

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,34 +1,17 @@
 return {
     { "arnamak/stay-centered.nvim", opts = {} },
     {
-        "crusj/bookmarks.nvim",
-        branch = "main",
-        keys = {
-            { "mw", mode = { "n" }, desc = "Toggle Bookmarks" },
+        "LintaoAmons/bookmarks.nvim",
+        dependencies = {
+            "stevearc/dressing.nvim", -- optional: to have the same UI shown in the GIF
         },
-        dependencies = { "nvim-web-devicons" },
-        config = function()
-            require("bookmarks").setup({
-                mappings_enabled = true, -- If the value is false, only valid for global keymaps: toggle、add、delete_on_virt、show_desc
-                keymap = {
-                    toggle = "mw", -- Toggle bookmarks(global keymap)
-                    close = "q", -- close bookmarks (buf keymap)
-                    add = "mm", -- Add bookmarks(global keymap)
-                    add_global = "mg", -- Add global bookmarks(global keymap), global bookmarks will appear in all projects. Identified with the symbol '󰯾'
-                    jump = "<CR>", -- Jump from bookmarks(buf keymap)
-                    delete = "dd", -- Delete bookmarks(buf keymap)
-                    order = "ms", -- Order bookmarks by frequency or updated_time(buf keymap)
-                    delete_on_virt = "md", -- Delete bookmark at virt text line(global keymap)
-                    show_desc = "mi", -- show bookmark desc(global keymap)
-                    focus_tags = "<c-j>", -- focus tags window
-                    focus_bookmarks = "<c-k>", -- focus bookmarks window
-                    toogle_focus = "<tab>", -- toggle window focus (tags-window <-> bookmarks-window)
-                },
-            })
-            require("telescope").load_extension("bookmarks")
-        end,
+        keys = {
+            { "mm", "<cmd>BookmarksMark<cr>", desc = "Mark current line into active BookmarkList." },
+            { "mg", "<cmd>BookmarksGoto<cr>", desc = "Go to bookmark at current active BookmarkList" },
+            { "mc", "<cmd>BookmarksCommands<cr>", desc = "Find and trigger a bookmark command." },
+            { "mo", "<cmd>BookmarksGotoRecent<cr>", desc = "Go to latest visited/created Bookmark" },
+        },
     },
-
     {
         "ghillb/cybu.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
