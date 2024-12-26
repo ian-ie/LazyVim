ba2f7fbff0f779d4bde22613c850c1ebde15d527
ianie <2817181586@qq.com>
2024-12-26 11:37:03 +0800

    dashboard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -29,6 +29,22 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     end,
 })
 
+vim.api.nvim_create_augroup("DashboardAutoOpen", { clear = true })
+vim.api.nvim_create_autocmd({ "BufDelete" }, {
+    group = "DashboardAutoOpen",
+    callback = function()
+        vim.schedule(function()
+            local buffers = vim.tbl_filter(function(buf)
+                return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= ""
+            end, vim.api.nvim_list_bufs())
+
+            if #buffers == 0 then
+                vim.cmd("lua Snacks.dashboard()")
+            end
+        end)
+    end,
+})
+
 vim.api.nvim_create_user_command("OpenInDOpus", function()
     -- 获取当前文件的完整路径
     local current_file = vim.fn.expand("%:p")
