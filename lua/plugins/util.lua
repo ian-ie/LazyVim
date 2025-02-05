b2dbfb49381cff16cdc5cc7176c4df8dd1a75af6
ianie <2817181586@qq.com>
2025-02-05 16:29:00 +0800

    update

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -13,6 +13,7 @@ return {
     {
         "shortcuts/no-neck-pain.nvim",
         version = "*",
+        opts = { buffers = { right = { enabled = false } } },
         keys = { { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "Zen Mode" } },
     },
 
