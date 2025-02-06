b3d101aa0f42bd1ef107bbf168eae36fc3bb7d40
ianie <2817181586@qq.com>
2025-02-06 18:12:46 +0800

    openai

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,40 @@
 return {
     {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- set this if you want to always pull the latest change
+        opts = {
+            provider = "openai",
+            openai = {
+                endpoint = "https://api.openai.com/v1",
+                model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+                timeout = 30000, -- timeout in milliseconds
+                temperature = 0, -- adjust if needed
+                max_tokens = 4096,
+            },
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.icons", -- or echasnovski/mini.icons
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    },
+    {
         "kawre/leetcode.nvim",
         build = ":TSUpdate html",
         opts = {
