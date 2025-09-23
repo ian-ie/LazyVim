dbd5100d50451a8059f426f15141317982581900
ianie <2817181586@qq.com>
2025-09-23 11:10:10 +0800

    cmp

--- lua/plugins/coding/fittencode.lua
+++ lua/plugins/coding/fittencode.lua
@@ -1,12 +1,11 @@
 return {
-  "luozhiya/fittencode.nvim",
-  config = function()
-    require("fittencode").setup({
-      completion_mode = "source",
-      inline_completion = {
-        enabled = true,
-      },
-    })
-  end,
+    "luozhiya/fittencode.nvim",
+    config = function()
+        require("fittencode").setup({
+            completion_mode = "source",
+            inline_completion = {
+                enabled = true,
+            },
+        })
+    end,
 }
-
--- lua/plugins/overrides/blink-cmp.lua
+++ lua/plugins/overrides/blink-cmp.lua
@@ -1,72 +1,91 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = {
-    {
-      "luozhiya/fittencode.nvim",
-      "Kaiser-Yang/blink-cmp-avante",
-      "mikavilpas/blink-ripgrep.nvim",
-    },
-  },
-  opts = {
-    keymap = {
-      preset = "enter",
-      ["<Tab>"] = {
-        LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-        "fallback",
-      },
-      ["<A-y>"] = require("minuet").make_blink_map(),
-      ["<C-j>"] = { "select_next", "fallback" },
-      ["<C-k>"] = { "select_prev", "fallback" },
-    },
-    sources = {
-      default = { "minuet", "avante", "lsp", "ripgrep", "path", "snippets", "buffer", "fittencode" },
-      -- completion = { trigger = { prefetch_on_insert = false } },
-      -- set custom providers with fittencode
-      providers = {
-        minuet = {
-          name = "minuet",
-          module = "minuet.blink",
-          async = true,
-          -- Should match minuet.config.request_timeout * 1000,
-          -- since minuet.config.request_timeout is in seconds
-          timeout_ms = 3000,
-          score_offset = 50, -- Gives minuet higher priority among suggestions
+    "saghen/blink.cmp",
+    dependencies = {
+        {
+            "luozhiya/fittencode.nvim",
+            "Kaiser-Yang/blink-cmp-avante",
+            "mikavilpas/blink-ripgrep.nvim",
         },
-        ripgrep = {
-          module = "blink-ripgrep",
-          name = "Ripgrep",
-          opts = {
-            prefix_min_len = 5,
-            toggles = {
-              -- The keymap to toggle the plugin on and off from blink
-              -- completion results. Example: "<leader>tg"
-              on_off = "<leader>tg",
+    },
+    opts = {
+        keymap = {
+            preset = "enter",
+            ["<Tab>"] = {
+                LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+                "fallback",
             },
-          },
-          transform_items = function(_, items)
-            for _, item in ipairs(items) do
-              -- example: append a description to easily distinguish rg results
-              item.labelDetails = {
-                description = "(rg)",
-              }
-            end
-            return items
-          end,
+            ["<A-y>"] = require("minuet").make_blink_map(),
+            ["<C-j>"] = { "select_next", "fallback" },
+            ["<C-k>"] = { "select_prev", "fallback" },
         },
-        avante = {
-          module = "blink-cmp-avante",
-          name = "Avante",
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
-        fittencode = {
-          name = "fittencode",
-          module = "fittencode.sources.blink",
-        },
-      },
-    },
-    completion = { trigger = { prefetch_on_insert = false } },
-  },
-}
+        sources = {
+            default = { "minuet", "avante", "lsp", "ripgrep", "path", "snippets", "buffer", "fittencode" },
+            -- completion = { trigger = { prefetch_on_insert = false } },
+            -- set custom providers with fittencode
+            providers = {
+                minuet = {
+                    name = "minuet",
+                    module = "minuet.blink",
+                    async = true,
+                    -- Should match minuet.config.request_timeout * 1000,
+                    -- since minuet.config.request_timeout is in seconds
+                    timeout_ms = 3000,
+                    score_offset = 50, -- Gives minuet higher priority among suggestions
 
+                    transform_items = function(_, items)
+                        for _, item in ipairs(items) do
+                            -- example: append a description to easily distinguish rg results
+                            item.labelDetails = {
+                                description = "🤖minuet",
+                            }
+                        end
+                        return items
+                    end,
+                },
+                ripgrep = {
+                    module = "blink-ripgrep",
+                    name = "Ripgrep",
+                    opts = {
+                        prefix_min_len = 5,
+                        toggles = {
+                            -- The keymap to toggle the plugin on and off from blink
+                            -- completion results. Example: "<leader>tg"
+                            on_off = "<leader>tg",
+                        },
+                    },
+                    transform_items = function(_, items)
+                        for _, item in ipairs(items) do
+                            -- example: append a description to easily distinguish rg results
+                            item.labelDetails = {
+                                description = "(rg)",
+                            }
+                        end
+                        return items
+                    end,
+                },
+                avante = {
+                    module = "blink-cmp-avante",
+                    name = "Avante",
+                    opts = {
+                        -- options for blink-cmp-avante
+                    },
+                },
+                fittencode = {
+                    name = "fittencode",
+                    module = "fittencode.sources.blink",
+
+                    transform_items = function(_, items)
+                        for _, item in ipairs(items) do
+                            -- example: append a description to easily distinguish rg results
+                            item.labelDetails = {
+                                description = "🔥fittencode",
+                            }
+                        end
+                        return items
+                    end,
+                },
+            },
+        },
+        completion = { trigger = { prefetch_on_insert = false } },
+    },
+}
