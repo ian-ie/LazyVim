5ee78b00d31bc20a827600d30c2fb3dd06096900
ianie <2817181586@qq.com>
2025-09-26 11:21:09 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -36,20 +36,20 @@ vim.api.nvim_create_autocmd("ModeChanged", {
 })
 
 vim.api.nvim_create_augroup("DashboardAutoOpen", { clear = true })
-vim.api.nvim_create_autocmd({ "BufDelete" }, {
-    group = "DashboardAutoOpen",
-    callback = function()
-        vim.schedule(function()
-            local buffers = vim.tbl_filter(function(buf)
-                return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= ""
-            end, vim.api.nvim_list_bufs())
-
-            if #buffers == 0 then
-                vim.cmd("lua Snacks.dashboard()")
-            end
-        end)
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "BufDelete" }, {
+--     group = "DashboardAutoOpen",
+--     callback = function()
+--         vim.schedule(function()
+--             local buffers = vim.tbl_filter(function(buf)
+--                 return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= ""
+--             end, vim.api.nvim_list_bufs())
+--
+--             if #buffers == 0 then
+--                 vim.cmd("lua Snacks.dashboard()")
+--             end
+--         end)
+--     end,
+-- })
 
 -- 创建一个自定义命令 :FormatJsonPaste
 vim.api.nvim_create_user_command("FormatJsonPaste", function()
